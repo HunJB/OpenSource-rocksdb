283425f4d3a41bcf2e3ca30c460b35ffde3b19b8
MSK1237 <kmins1324@gmail.com>
2026-05-11 13:00:33 +0900

    Update aggregate.py

--- experiment/aggregate.py
+++ experiment/aggregate.py
@@ -1,7 +1,7 @@
 # 수정 : adv 통합 추가 수행
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.ticker as. mticker
+import matplotlib.ticker as mticker
 import numpy as np
 import os
 import glob
